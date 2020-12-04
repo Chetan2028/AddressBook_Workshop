2edba0cba42e761d1eee788d5d786844b5a42a0b
Chetan2028 <bmchetan2028@gmail.com>
2020-12-04 15:00:15 +0530

    [Chetan] Add : Added contact to list

---
+++ AddressBook_Workshop/AddressBook.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook_Workshop
+{
+    public class AddressBook
+    {
+        List<Contact> contactList = new List<Contact>();
+
+        public void AddContact()
+        {
+            try
+            {
+                ContactValidator validator = new ContactValidator();
+
+                Console.WriteLine("Enter First Name");
+                string firstName = Console.ReadLine();
+                validator.ValidateFirstName(firstName);
+
+                Console.WriteLine("Enter Last Name");
+                string lastName = Console.ReadLine();
+                validator.ValidateLastName(lastName);
+
+                Console.WriteLine("Enter Address");
+                string address = Console.ReadLine();
+                validator.ValidateAddress(address);
+
+                Console.WriteLine("Enter Zip");
+                int zip = Convert.ToInt32(Console.ReadLine());
+                validator.ValidateZip(zip);
+
+                Console.WriteLine("Enter Phone Number");
+                string phoneNumber = Console.ReadLine();
+                validator.ValidatePhoneNumber(phoneNumber);
+
+                Console.WriteLine("Enter City");
+                string city = Console.ReadLine();
+                validator.ValidateCity(city);
+
+                Console.WriteLine("Enter State");
+                string state = Console.ReadLine();
+                validator.ValidateState(state);
+
+                Console.WriteLine("Enter Your Email");
+                string email = Console.ReadLine();
+                validator.ValidateEmail(email);
+
+
+                Contact contact = new Contact
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Address = address,
+                    City = city,
+                    State = state,
+                    Zip = zip,
+                    PhoneNumber = phoneNumber,
+                    Email = email
+                };
+
+                contactList.Add(contact);
+            }
+            catch (AddressBookCustomException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

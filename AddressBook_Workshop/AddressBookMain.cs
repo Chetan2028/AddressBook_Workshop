3b0d0425853f69f7058624f16519d6726010c119
Chetan2028 <bmchetan2028@gmail.com>
2020-12-04 13:46:57 +0530

    Add project files.

---
+++ AddressBook_Workshop/AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_Workshop
+{
+    public class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Problem");
+        }
+    }
+}
